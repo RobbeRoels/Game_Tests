bd7d920b57444beef5d4257fe1272bf5ffa2a869
Robbe Roels <robbe.roels@hotmail.com>
2015-10-31 11:14:50 +0100

    Basic board generator/ moved folders
    Basic board generator imported from tutorial, moved around folders

--- Assets/Lookup scripts/Completed/Scripts/GameManager.cs
+++ Assets/Lookup scripts/Completed/Scripts/GameManager.cs
@@ -17,7 +17,7 @@ namespace Completed
 		
 		private Text levelText;									//Text to display current level number.
 		private GameObject levelImage;							//Image to block out level as levels are being set up, background for levelText.
-		private BoardManager boardScript;						//Store a reference to our BoardManager which will set up the level.
+		//private BoardManager boardScript;						//Store a reference to our BoardManager which will set up the level.
 		private int level = 1;									//Current level number, expressed in game as "Day 1".
 		private List<Enemy> enemies;							//List of all Enemy units, used to issue them move commands.
 		private bool enemiesMoving;								//Boolean to check if enemies are moving.
@@ -47,7 +47,7 @@ namespace Completed
 			enemies = new List<Enemy>();
 			
 			//Get a component reference to the attached BoardManager script
-			boardScript = GetComponent<BoardManager>();
+			//boardScript = GetComponent<BoardManager>();
 			
 			//Call the InitGame function to initialize the first level 
 			InitGame();
@@ -85,15 +85,15 @@ namespace Completed
 			
 			//Clear any Enemy objects in our List to prepare for next level.
 			enemies.Clear();
-			
-			//Call the SetupScene function of the BoardManager script, pass it current level number.
-			boardScript.SetupScene(level);
-			
-		}
-		
-		
-		//Hides black image used between levels
-		void HideLevelImage()
+
+            //Call the SetupScene function of the BoardManager script, pass it current level number.
+            //boardScript.SetupScene(level);
+
+        }
+
+
+        //Hides black image used between levels
+        void HideLevelImage()
 		{
 			//Disable the levelImage gameObject.
 			levelImage.SetActive(false);
