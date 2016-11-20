10378ad8343edfa8dade999eeb82ce8e8f507ef5
Robbe Roels <robbe.roels@gmail.com>
2016-11-20 16:54:51 +0100

    Added: Running and stamina
    Added running when holding left shift. Added stamina counter TODO:
    Change into stamina bar.

--- Assets/Scripts/Player/MovePlayer.cs
+++ Assets/Scripts/Player/MovePlayer.cs
@@ -3,7 +3,17 @@ using System.Collections;
 
 public class MovePlayer : MonoBehaviour {
 
-	public float maxSpeed = 20.0f;
+	public float walkSpeed = 3.5f;
+	public float runSpeed = 5f;
+
+
+	public float maxStamina = 100.0f;
+	public float stamina = 100.0f;
+	public float staminaUsage = 10f;
+	public float staminaRegen = 7.5f;
+
+	public bool exhausted = false;
+
 
     public delegate void ExitReached();
     public static event ExitReached OnExitReached;
@@ -15,13 +25,53 @@ public class MovePlayer : MonoBehaviour {
 
 	
 	void ManageMovement(){
+		bool running = false;
 		float inputX = Input.GetAxis ("Horizontal");
 		float inputY = Input.GetAxis("Vertical");
-		if(Input.GetButton("Horizontal") || Input.GetButton("Vertical")){
+		float speed = walkSpeed;
+		if (Input.GetKey (KeyCode.LeftShift)) {
+			if (stamina > 0) {
+				if (!exhausted || stamina > 30) {
+					exhausted = false;
+					speed = runSpeed;
+					running = true;
+				}
+			} else {
+				exhausted = true;
+			}
+		}
+
+		if (Input.GetButton ("Horizontal") || Input.GetButton ("Vertical")) {
 			//Vector3 direction = transform.up*inputX + transform.right*inputY; //Direction relative to rotation
-			Vector3 direction = new Vector3 (inputX, inputY,0); //Direction relative to screen
+			Vector3 direction = new Vector3 (inputX, inputY, 0); //Direction relative to screen
 			direction.Normalize ();
-			transform.position = transform.position + direction*(maxSpeed*Time.deltaTime);
+			transform.position = transform.position + direction * (speed * Time.deltaTime);
+			if (running) {
+				lowerStamina ();
+			} else {
+				regenStamina (true);
+			}
+		} else {
+			regenStamina (false);
+		}
+
+
+	}
+
+	void lowerStamina(){
+		stamina -= (Time.deltaTime * staminaUsage);
+		if (stamina < 0) {
+			stamina = 0;
+		}
+	}
+	void regenStamina(bool moving){			
+		if (!moving) {
+			stamina += (Time.deltaTime * (staminaRegen));
+		} else {
+			stamina += (Time.deltaTime * (staminaRegen* 0.5f));
+		}
+		if (stamina > maxStamina) {
+			stamina = maxStamina;
 		}
 	}
 
---
+++ Assets/Scripts/UI/UIManager.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class UIManager : MonoBehaviour {
+	GameObject UI;
+
+	void awake(){
+		UI = GameObject.Find ("UICanvas");
+	}
+
+	// Update is called once per frame
+	void Update () {
+		if (!(GameObject.FindGameObjectWithTag ("Player") == null)) {
+			float stamina = GameObject.FindGameObjectWithTag ("Player").GetComponent<MovePlayer> ().stamina;
+			GameObject staminaText = GameObject.Find ("StaminaText");
+			staminaText.GetComponent<Text> ().text = string.Format ("Stamina: {0:0}", stamina);
+			if (stamina == 0) {
+				staminaText.GetComponent<Text> ().color = Color.red;
+			} else {
+				if (stamina >= 30) {
+					staminaText.GetComponent<Text> ().color = Color.white;
+				}
+			}
+		}
+	}
+}
