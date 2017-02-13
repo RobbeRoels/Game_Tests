4e706137b1ab1fc5a7681d7730166a9dde5d8461
Robbe Roels <robbe.roels@hotmail.com>
2017-02-13 23:48:30 +0100

    Added shadows, cleaned up files
    Added shadows, added "invisible" cube object to walls.
    Cleaned up old lookup files.

---
+++ Assets/Scripts/UI/ShadowScript.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ShadowScript : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+        SpriteRenderer sr = gameObject.GetComponent<SpriteRenderer>();
+        sr.receiveShadows = true;
+    }
+
+    // Update is called once per frame
+    void Update () {
+	
+	}
+}
